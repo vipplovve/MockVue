fae1cc592bc1fd6fb6fae65705b1235ad0d020b8
Samar-28 <sampahwa28@gmail.com>
2025-02-17 20:03:48 +0530

    Backend init

---
+++ Backend/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send("Hello");
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
